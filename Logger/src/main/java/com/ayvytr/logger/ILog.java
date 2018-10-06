16943d1e182a05759da0451af8e54265f213b8f4
wangdunwei <wangdunwei@wxbtc.com>
2018-10-06 11:16:05 +0800

    添加注释

--- Logger/src/main/java/com/ayvytr/logger/ILog.java
+++ Logger/src/main/java/com/ayvytr/logger/ILog.java
@@ -2,37 +2,50 @@ package com.ayvytr.logger;
 
 /**
  * Log接口
+ *
  * @author Ayvytr ['s GitHub](https://github.com/Ayvytr)
  * @since 1.0.0
  */
 public interface ILog
 {
     /**
+     * @param tag     Tag
+     * @param message Message
      * @see android.util.Log#d(String, String)
      */
     void d(String tag, String message);
 
     /**
+     * @param tag     Tag
+     * @param message Message
      * @see android.util.Log#e(String, String)
      */
     void e(String tag, String message);
 
     /**
+     * @param tag     Tag
+     * @param message Message
      * @see android.util.Log#w(String, String)
      */
     void w(String tag, String message);
 
     /**
+     * @param tag     Tag
+     * @param message Message
      * @see android.util.Log#i(String, String)
      */
     void i(String tag, String message);
 
     /**
+     * @param tag     Tag
+     * @param message Message
      * @see android.util.Log#v(String, String)
      */
     void v(String tag, String message);
 
     /**
+     * @param tag     Tag
+     * @param message Message
      * @see android.util.Log#wtf(String, String)
      */
     void wtf(String tag, String message);
